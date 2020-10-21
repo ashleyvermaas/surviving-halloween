deff02ba63eced48d42299e3e087b466bf3c4969
Ashley Vermaas <ashleyvermaas1@gmail.com>
2020-10-21 12:53:10 +0200

    add player functionality up and down

--- js/canvas.js
+++ js/canvas.js
@@ -2,16 +2,33 @@ const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
 class Player {
-    constructor(posX, posY, radius) {
-        this.x = posX;
+    constructor(/*posX, posY, radius*/) {
+        /*this.x = posX;
         this.y = posY;
-        this.radius = radius;
+        this.radius = radius;*/
+
+        this.x = 0;
+        this.y = 400;
     }
 
     createPlayer() {
         ctx.fillStyle = 'black';
-        ctx.fillRect(0, 0, 100, 100);
+        //ctx.fillRect(0, 400, 100, 100);
+        ctx.fillRect(this.x, this.y, 100, 100);
     }
+
+    jump(){
+        this.y -= 100;
+}
+
+    moveUp(){
+        this.y -= 10;
+    }
+
+    moveDown(){
+        this.y += 10;
+    }
+
 }
 
 
@@ -24,7 +41,7 @@ class Obstacle {
 
     createObstacle() {
         ctx.fillStyle = 'red';
-        ctx.fillRect(300, 0, 100, 100);
+        ctx.fillRect(300, 400, 100, 100);
     }
 }
 
@@ -37,15 +54,16 @@ class Target {
 
     createTarget() {
         ctx.fillStyle = 'green';
-        ctx.fillRect(600, 0, 100, 100);
+        ctx.fillRect(0, 0, 100, 100);
     }
 }
 
 // Show player, obstacle and target on canvas
-let player = new Player();
-let obstacle = new Obstacle();
-let target = new Target();
+const player = new Player();
+const obstacle = new Obstacle();
+const target = new Target();
 
 player.createPlayer();
 obstacle.createObstacle();
 target.createTarget();
+
--- js/game.js
+++ js/game.js
@@ -1,3 +1,30 @@
 class Game {
-    
+
+}
+
+
+
+document.addEventListener('keydown', e => {
+    switch (e.keyCode) {
+        case 16:
+            player.jump();
+            console.log('jump', player);
+            break;
+        case 38:
+            player.moveUp();
+            console.log('up', player);
+            break;
+        case 40:
+            player.moveDown();
+            console.log('down', player);
+            break;
+    }
+    updateCanvas();
+});
+
+function updateCanvas() {
+    ctx.clearRect(0, 0, 1000, 500);
+    player.createPlayer();
+    obstacle.createObstacle();
+    target.createTarget();
 }
